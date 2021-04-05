15dcb1dc4456611556eb3d510038d19dba16f788
Yelidá <Yelid@LAPTOP-5QVP985L>
2021-04-05 17:32:10 -0400

    -Registro Proveedor

--- ISC212-ProyectoFinal/src/visual/RegistroProovedor.java
+++ ISC212-ProyectoFinal/src/visual/RegistroProovedor.java
@@ -7,10 +7,22 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import java.awt.Toolkit;
 
 public class RegistroProovedor extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField txtRNC;
+	private JTextField txtNombre;
+	private JTextField txtDireccion;
+	private JTextField txtTelefono;
+	private JTextField txtRepresentante;
 
 	/**
 	 * Launch the application.
@@ -29,27 +41,81 @@ public class RegistroProovedor extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegistroProovedor() {
-		setBounds(100, 100, 450, 300);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(RegistroProovedor.class.getResource("/resources/provee.png")));
+		setTitle("Registro de Proveedores");
+		setResizable(false);
+		setBounds(100, 100, 516, 454);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Registro de Proveedores", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(null);
+			
+			JLabel lblRNC = new JLabel("RNC:");
+			lblRNC.setBounds(10, 51, 124, 14);
+			panel.add(lblRNC);
+			
+			JLabel lblNewLabel = new JLabel("Nombre de la empresa:");
+			lblNewLabel.setBounds(10, 116, 172, 14);
+			panel.add(lblNewLabel);
+			
+			JLabel lblNewLabel_1 = new JLabel("Direcci\u00F3n:");
+			lblNewLabel_1.setBounds(10, 181, 124, 14);
+			panel.add(lblNewLabel_1);
+			
+			JLabel lblNewLabel_2 = new JLabel("Tel\u00E9fono:");
+			lblNewLabel_2.setBounds(10, 246, 124, 14);
+			panel.add(lblNewLabel_2);
+			
+			JLabel lblNewLabel_3 = new JLabel("Representante:");
+			lblNewLabel_3.setBounds(10, 311, 124, 14);
+			panel.add(lblNewLabel_3);
+			
+			txtRNC = new JTextField();
+			txtRNC.setBounds(153, 46, 138, 20);
+			panel.add(txtRNC);
+			txtRNC.setColumns(10);
+			
+			txtNombre = new JTextField();
+			txtNombre.setBounds(153, 112, 275, 20);
+			panel.add(txtNombre);
+			txtNombre.setColumns(10);
+			
+			txtDireccion = new JTextField();
+			txtDireccion.setBounds(153, 178, 275, 20);
+			panel.add(txtDireccion);
+			txtDireccion.setColumns(10);
+			
+			txtTelefono = new JTextField();
+			txtTelefono.setBounds(153, 244, 138, 20);
+			panel.add(txtTelefono);
+			txtTelefono.setColumns(10);
+			
+			txtRepresentante = new JTextField();
+			txtRepresentante.setBounds(153, 310, 275, 20);
+			panel.add(txtRepresentante);
+			txtRepresentante.setColumns(10);
+		}
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton btnRegistrar = new JButton("Registrar");
+				btnRegistrar.setActionCommand("OK");
+				buttonPane.add(btnRegistrar);
+				getRootPane().setDefaultButton(btnRegistrar);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JButton btnCancelar = new JButton("Cancelar");
+				btnCancelar.setActionCommand("Cancel");
+				buttonPane.add(btnCancelar);
 			}
 		}
 	}
-
 }
